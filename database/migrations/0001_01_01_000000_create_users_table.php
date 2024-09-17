3dce7fee54c0699c46153b26961a17e0194936cd
shahadat1010 <19101071@uap-bd.edu>
2024-09-17 12:35:30 +0600

    Update 0001_01_01_000000_create_users_table.php
    version final

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -40,6 +44,22 @@ return new class extends Migration
             $table->integer('last_activity')->index();
         });
     }
+    class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeders.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@example.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
+
+
 
     /**
      * Reverse the migrations.
